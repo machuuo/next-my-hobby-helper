8cf48e40d0edf32f5fd4feab2da9c85a245874f5
shark <seonjae1003@naver.com>
2025-03-25 10:41:27 +0900

    25.03.24 commit

--- components/organisms/dropdown/Dropdown.tsx
+++ components/organisms/dropdown/Dropdown.tsx
@@ -1,39 +1,21 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
 import { DropdownProps } from "@/types/Props";
 import { BaseButton } from "@/components/atoms/button";
 import styles from "./Dropdown.module.css";
+import { useDropdown } from "@/hooks/useDropdown";
 
 export default function Dropdown({
   buttonContent,
   items,
   className,
 }: DropdownProps) {
-  const dropdownRef = useRef<HTMLDivElement>(null);
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const { ref, isOpen, toggle, close } = useDropdown<HTMLDivElement>({});
 
-  const toggleDropdown = () => {
-    setIsOpen((prev) => !prev);
-  };
-
-  useEffect(() => {
-    // const handleClickOutside = (e: MouseEvent) => {
-    //   if (
-    //     dropdownRef.current &&
-    //     !dropdownRef.current.contains(e.target as Node)
-    //   ) {
-    //     setIsOpen(false);
-    //   }
-    // };
-    // document.addEventListener("mouseup", handleClickOutside);
-    // return () => {
-    //   document.removeEventListener("mouseup", handleClickOutside);
-    // };
-  }, []);
+  const toggleDropdown = () => toggle();
 
   return (
-    <div className={`${styles.dropdown} ${className || ""}`} ref={dropdownRef}>
+    <div className={`${styles.dropdown} ${className || ""}`} ref={ref}>
       <BaseButton className={styles.button} onClick={toggleDropdown}>
         {buttonContent}
       </BaseButton>
@@ -44,7 +26,7 @@ export default function Dropdown({
               <BaseButton
                 onClick={() => {
                   item.onClick();
-                  setIsOpen(false);
+                  close();
                 }}
                 className={styles.menuButton}
               >
